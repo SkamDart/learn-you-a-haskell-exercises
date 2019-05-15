05e380bb9b9b617bd735e84c74c04e3bee36d996
Cameron Dart <camerondart13@gmail.com>
2019-05-14 21:52:50 -0700

    a few more recursion

--- lib/Exercises/Recursion.hs
+++ lib/Exercises/Recursion.hs
@@ -16,23 +16,28 @@ power x y = power' x y 1
 -- powers based on type signature.
 power' :: Int -> Int -> Int -> Int
 power' x y acc
---  | y < 0 = power' x (y + 1) (acc * (1 / x))
+  | y < 0 = error "Unable to take integer power to negative number"
   | y > 0 = power' x (y - 1) (acc * x)
   | otherwise = acc
 
-fib :: (Num a, Eq a) => a -> [a]
-fib x
-  | x == 0 = [0]
-  | x == 1 = [1, 0]
-  | otherwise = rest ++ [1, 0]
-  where rest = []
+fib :: (Num a, Enum a, Eq a) => a -> [a]
+fib a = [fib' i | i <- [0..a] ]
 
-stopReverseSign :: (Fractional a, Ord a) => a -> a-> a
-stopReverseSign = undefined
+fib' :: (Num a, Enum a, Eq a) => a -> a
+fib' a
+    | a == 0 = 0
+    | a == 1 = 1
+    | otherwise = fib' (a - 1) + fib' (a - 2)
+
+
+stopReverseSign :: (Fractional a, Ord a) => a -> a -> a
+stopReverseSign a step
+    | a < 0 = (((-1) * a) + step)
+    | a > 0 = (-1) * (a + step)
+    | otherwise = 0
 
 piCalc :: (Fractional a, Integral b, Ord a) => a -> (a, b)
 piCalc a = undefined
-
 
 piCalc' :: (Ord a, Fractional a, Integral b) => a -> a -> a -> b -> (a, b)
 piCalc' w x y z = undefined
